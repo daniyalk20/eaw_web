ba01c54555c0264f4374a6c1ae107810d89149d7
Daniyal Khan <daniyalatjit@gmail.com>
2022-06-01 17:53:15 -0400

    Init Back-End

---
+++ eaw_front_end/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React  from 'react';
+// import Badge from '@mui/material/Badge';
+// import MailIcon from '@mui/icons-material/Mail';
+
+function App() {
+  return (
+  <>
+    <div className="App">
+      <h1>Intialized Web</h1>
+    </div>
+  </>
+  );
+}
+
+export default App;
